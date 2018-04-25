6f3b9b53710f0aac16fa111cc1b2ab62b789d451
RichardSyfer <deadwalker87@gmail.com>
2018-04-25 16:05:36 +0300

    162 koans completed

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,24 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  raise TriangleError, 'Sides lengths should be greater than 0' if a<=0 or b<=0 or c<=0
+
+  sidesArr = [a,b,c]
+  longestSide = sidesArr.max
+  perimeter = 0
+  sidesArr.each { |side| perimeter += side }
+  if perimeter - longestSide <= longestSide
+    raise TriangleError, "It isn't triangle. Two sides longer than longest one"
+  end
+
+  if a==b and a==c
+    :equilateral
+  elsif a!=b and b!=c and a!=c
+    :scalene
+  else
+    :isosceles
+  end
+
 end
 
 # Error class used in part 2.  No need to change this code.
